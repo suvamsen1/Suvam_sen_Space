3e56c5d92e56e6e3be33709a9bfb5d27075d016d
Suvam Sen <121876820+suvamsen1@users.noreply.github.com>
2026-09-06 12:11:37 +0000

    codebase cleanup

--- Suvam_ML/Suvam_Practice1/Q1.py
+++
@@ -1,7 +0,0 @@
-"""
-1. Write a Python program to input the height of the person and check if the height of the
-person is greater than or equal to 6 feet then print the message “The person is tall”.  
-"""
-a = int(input("Enter Height: "))
-if(a >= 6):
-    print("\"Person is tall\"")
--- Suvam_ML/Suvam_Practice1/Q3.py
+++
@@ -1,9 +0,0 @@
-"""
-3. Input an integer through the keyboard. Write a Python program to find out whether it is
-an odd number or even number.
-"""
-a = int(input("Enter your marks: "))
-if(a%2 == 0):
-    print(a," is Even number")
-else:
-    print(a," is odd number")
